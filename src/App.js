1c91bce5199f79d4f28ed5931885efad51c9a5cb
Michel Migalov <mafri.michel@gmail.com>
2022-09-19 01:19:52 +0300

    Add data from backend

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 import pizzas from "./pizzas.json";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63278ddb9a053ff9aaa74737.mockapi.io/items')
+      .then(res => res.json())
+      .then(arr => setItems(arr));
+  }, [])
+
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((obj) => 
+              items.map((obj) => 
                 <PizzaBlock key={obj.id} {...obj} />
               )
             }
