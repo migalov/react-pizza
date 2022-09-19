48301c710aa823d8456c6ab863ba239a7c9c73d0
Michel Migalov <mafri.michel@gmail.com>
2022-09-19 17:14:41 +0300

    Search setting through frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,29 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import PizzaBlockSkeleton from "../components/PizzaBlock/Skeleton";
 
-export const Home = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [categoryId, setCategoryId] = useState(0);
-  const [sortType, setSortType] = useState({
-    name: "популярности",
-    sortProperty: "rating",
-  });
+export const Home = ({ searchValue }) => {
+  const [items, setItems] = useState([]),
+    [isLoading, setIsLoading] = useState(true),
+    [categoryId, setCategoryId] = useState(0),
+    [sortType, setSortType] = useState({
+      name: "популярности",
+      sortProperty: "rating",
+    }),
+    pizzas = items
+      .filter((obj) => {
+        if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+          return true;
+        }
+        else return false;
+      })
+      .map((obj) => <PizzaBlock key={obj.id} {...obj} />),
+    skeletons = [...new Array(6)].map((key) => <PizzaBlockSkeleton />);
 
   useEffect(() => {
     setIsLoading(true);
-    const   order = sortType.sortProperty.includes('-') ? "asc" : "desc",
-            sortBy = sortType.sortProperty.replace('-',''),
-            category = categoryId > 0 ? `category=${categoryId}` : "";
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc",
+      sortBy = sortType.sortProperty.replace("-", ""),
+      category = categoryId > 0 ? `category=${categoryId}` : "";
     fetch(
       `https://63278ddb9a053ff9aaa74737.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
     )
@@ -40,11 +49,7 @@ export const Home = () => {
         <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((key) => <PizzaBlockSkeleton />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
     </>
   );
 };
